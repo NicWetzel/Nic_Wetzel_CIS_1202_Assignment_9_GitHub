113ce2827e2b00b6ab5a79ff976e4cde1768e2ce
NicWetzel <57080630+NicWetzel@users.noreply.github.com>
2021-10-23 18:44:56 -0400

    Add files via upload

---
+++ Nic_Wetzel_CIS_1202_Assignment_9_GitHub.cpp
@@ -0,0 +1,12 @@
+// Nic_Wetzel_CIS_1202_Assignment_9_GitHub.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
